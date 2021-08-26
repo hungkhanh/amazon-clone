7137d037ea17b74eec45c88cd6856e7555bae446
hungpk <hungpk1412@gmail.com>
2021-08-26 10:00:07 +0700

    Add UI/Header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBTi4mRC-0qoSdW_VGSEZ0vJueW602FoKk",
+    authDomain: "challenge-fc83a.firebaseapp.com",
+    projectId: "challenge-fc83a",
+    storageBucket: "challenge-fc83a.appspot.com",
+    messagingSenderId: "120977852828",
+    appId: "1:120977852828:web:95a1987cd05bc7d1b3a059",
+    measurementId: "G-5SJ75SZKGJ"
+  };
